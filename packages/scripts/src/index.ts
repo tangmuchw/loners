b86054e16a3b30f196bad17c13f29c21930aefe6
chenwei <chenwei@zhihu.com>
2021-07-02 15:26:21 +0800

    chore(scripts): 修改 scripts 的文件名

---
+++ packages/scripts/src/index.ts
@@ -0,0 +1,30 @@
+import moment from 'moment';
+
+/**
+ * @param seconds 为数字时，代表时间戳，单位为: s
+ * @param rule
+ */
+const formatTime = (seconds?: string | number, rule = 'YYYY-MM-DD HH:mm:ss') => {
+  if (!seconds) return '';
+
+  if (typeof seconds === 'number') return moment(seconds * 1000).format(rule);
+
+  return moment(seconds, 'YYYYMMDDHHmmss').format(rule);
+};
+
+const isMobile = () => /(iPhone|iPad|iPod|iOS|Android)/i.test(window.navigator.userAgent);
+
+/** 格式化数字为 xxx,xxx,xxx */
+const formatNumToThousands = (value?: number) => {
+  if (value === undefined || value === null) return '';
+
+  return !Number.isNaN(value)
+    ? value?.toString().replace(new RegExp(/(?<=-?\d)(?=(\d{3})+(\.{1}\d+)?$)/, 'g'), ',')
+    : value;
+};
+
+export default {
+  formatNumToThousands,
+  isMobile,
+  formatTime,
+};
