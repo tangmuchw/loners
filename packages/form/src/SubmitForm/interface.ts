9b5fdf773c7b19a18da49c4a58ec92545c642e7e
chenwei <chenwei@zhihu.com>
2021-07-05 17:07:50 +0800

    chore(form): 调整 title 的类型定义-SubmitForm

--- packages/form/src/SubmitForm/interface.ts
+++ packages/form/src/SubmitForm/interface.ts
@@ -1,10 +1,10 @@
 import type { FormWidgetProps } from '../FormWidget/interface';
 import type { FormToolProps, ShowFunc } from '../FormTool/interface';
-import type { FormInstance } from 'antd';
+import type { ReactNode } from 'react';
 
 export type FormGroup = {
   /** 一个 FormGroup 的标题，例如：基础设置 */
-  title?: string;
+  title?: ReactNode;
 
   /** 多个 FormItem 的配置 */
   items: FormToolProps['items'];
