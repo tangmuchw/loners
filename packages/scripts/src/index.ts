15e21adcd6e9887508013d9d88e987b9a3fc1b89
chenwei <chenwei@zhihu.com>
2021-07-05 14:45:59 +0800

    chore(scripts): 修改文件结构

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,28 +1,5 @@
-import moment from 'moment';
+import { formatNumToThousands, formatTime, isMobile } from './utils';
 import type { LonersScripts } from './interface';
-
-/**
- * @param seconds 为数字时，代表时间戳，单位为: s
- * @param rule
- */
-const formatTime = (seconds?: string | number, rule = 'YYYY-MM-DD HH:mm:ss') => {
-  if (!seconds) return '';
-
-  if (typeof seconds === 'number') return moment(seconds * 1000).format(rule);
-
-  return moment(seconds, 'YYYYMMDDHHmmss').format(rule);
-};
-
-const isMobile = () => /(iPhone|iPad|iPod|iOS|Android)/i.test(window.navigator.userAgent);
-
-/** 格式化数字为 xxx,xxx,xxx */
-const formatNumToThousands = (num?: number) => {
-  if (num === undefined || num === null) return '';
-
-  return !Number.isNaN(num)
-    ? num?.toString().replace(new RegExp(/(?<=-?\d)(?=(\d{3})+(\.{1}\d+)?$)/, 'g'), ',')
-    : `${num}`;
-};
 
 const lonersScripts: LonersScripts = {
   formatNumToThousands,
---
+++ packages/scripts/src/utils.ts
@@ -0,0 +1,24 @@
+import moment from 'moment';
+
+/**
+ * @param seconds 为数字时，代表时间戳，单位为: s
+ * @param rule
+ */
+export const formatTime = (seconds?: string | number, rule = 'YYYY-MM-DD HH:mm:ss') => {
+  if (!seconds) return '';
+
+  if (typeof seconds === 'number') return moment(seconds * 1000).format(rule);
+
+  return moment(seconds, 'YYYYMMDDHHmmss').format(rule);
+};
+
+export const isMobile = () => /(iPhone|iPad|iPod|iOS|Android)/i.test(window.navigator.userAgent);
+
+/** 格式化数字为 xxx,xxx,xxx */
+export const formatNumToThousands = (num?: number) => {
+  if (num === undefined || num === null) return '';
+
+  return !Number.isNaN(num)
+    ? num?.toString().replace(new RegExp(/(?<=-?\d)(?=(\d{3})+(\.{1}\d+)?$)/, 'g'), ',')
+    : `${num}`;
+};
